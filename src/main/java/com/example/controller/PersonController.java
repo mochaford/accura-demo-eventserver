b0389c5a31f0c1f16ba1b2ef194baa0866e6ab2c
Oak <598823087@qq.com>
2017-01-16 11:18:57 +0800

    Update PersonController.java

--- src/main/java/com/example/controller/PersonController.java
+++ src/main/java/com/example/controller/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.model.Person;
+import com.example.model.PioCylinderHistory;
 import com.example.service.PersonService;
 
 import java.util.Map;
