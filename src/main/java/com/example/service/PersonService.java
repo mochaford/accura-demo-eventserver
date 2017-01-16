5b73ac753be5fd30c1aa05bb09e42bc44cfcf83d
Oak <598823087@qq.com>
2017-01-16 11:09:49 +0800

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -9,5 +9,7 @@ public interface PersonService {
     
     public void addPerson(Person person);
     public List<Person> listPeople();
+    public void addPerson(PioCylinderHistory history);
+    public List<Person> listHistory();
     public void removePerson(Integer id);
 }
